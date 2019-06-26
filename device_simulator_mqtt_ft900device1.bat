d29e2b574d2f21aac1a75288a0f97be2e2882b06
richmondu <richmond.umagat@yahoo.com>
2019-06-26 21:31:58 +0800

    Add pause on batch files

--- device_simulator_mqtt_ft900device1.bat
+++ device_simulator_mqtt_ft900device1.bat
@@ -4,3 +4,4 @@ set DEVICE_CERT="cert/ft900device1_cert.pem"
 set DEVICE_PKEY="cert/ft900device1_pkey.pem"
 
 python.exe device_simulator.py --USE_AMQP 0 --USE_DEVICE_NAME %DEVICE_NAME% --USE_DEVICE_CA %DEVICE_CA% --USE_DEVICE_CERT %DEVICE_CERT% --USE_DEVICE_PKEY %DEVICE_PKEY%
+pause
