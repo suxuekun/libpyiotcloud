477638092f33a6e4fea7851eac51935f6b25c400
richmondu <richmond.umagat@yahoo.com>
2019-06-26 21:19:03 +0800

    Specify CA file

--- device_simulator_mqtt_ft900device1.bat
+++ device_simulator_mqtt_ft900device1.bat
@@ -1,5 +1,6 @@
 set DEVICE_NAME="ft900device1"
+set DEVICE_CA="cert/rootca.pem"
 set DEVICE_CERT="cert/ft900device1_cert.pem"
 set DEVICE_PKEY="cert/ft900device1_pkey.pem"
 
-python.exe device_simulator.py --USE_AMQP 0 --USE_DEVICE_NAME %DEVICE_NAME% --USE_DEVICE_CERT %DEVICE_CERT% --USE_DEVICE_PKEY %DEVICE_PKEY%
+python.exe device_simulator.py --USE_AMQP 0 --USE_DEVICE_NAME %DEVICE_NAME% --USE_DEVICE_CA %DEVICE_CA% --USE_DEVICE_CERT %DEVICE_CERT% --USE_DEVICE_PKEY %DEVICE_PKEY%
