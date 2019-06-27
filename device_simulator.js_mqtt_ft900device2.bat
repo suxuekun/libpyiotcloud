ba5b543f16dd32352b41b94a812cbd58d87d9bd4
richmondu <richmond.umagat@yahoo.com>
2019-06-27 22:45:05 +0800

    Added batch script for NodeJS MQTT device simulator

---
+++ device_simulator.js_mqtt_ft900device2.bat
@@ -0,0 +1,7 @@
+set DEVICE_NAME="ft900device2"
+set DEVICE_CA="cert/rootca.pem"
+set DEVICE_CERT="cert/ft900device2_cert.pem"
+set DEVICE_PKEY="cert/ft900device2_pkey.pem"
+
+node device_simulator.js --USE_DEVICE_NAME %DEVICE_NAME% --USE_DEVICE_CA %DEVICE_CA% --USE_DEVICE_CERT %DEVICE_CERT% --USE_DEVICE_PKEY %DEVICE_PKEY%
+pause
