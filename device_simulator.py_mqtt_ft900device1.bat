c5a851a1053d0a65fbc2b50cbc17965deb6a3a03
richmondu <richmond.umagat@yahoo.com>
2019-06-28 15:34:12 +0800

    Update devicename to deviceid

--- device_simulator.py_mqtt_ft900device1.bat
+++ device_simulator.py_mqtt_ft900device1.bat
@@ -1,7 +1,7 @@
-set DEVICE_NAME="ft900device1"
+set DEVICE_ID="c2bfb6ec02f4bb0cb146fe4b2c7752a90d02f79d"
 set DEVICE_CA="cert/rootca.pem"
 set DEVICE_CERT="cert/ft900device1_cert.pem"
 set DEVICE_PKEY="cert/ft900device1_pkey.pem"
 
-python.exe device_simulator.py --USE_AMQP 0 --USE_DEVICE_NAME %DEVICE_NAME% --USE_DEVICE_CA %DEVICE_CA% --USE_DEVICE_CERT %DEVICE_CERT% --USE_DEVICE_PKEY %DEVICE_PKEY%
+python.exe device_simulator.py --USE_AMQP 0 --USE_DEVICE_ID %DEVICE_ID% --USE_DEVICE_CA %DEVICE_CA% --USE_DEVICE_CERT %DEVICE_CERT% --USE_DEVICE_PKEY %DEVICE_PKEY%
 pause
