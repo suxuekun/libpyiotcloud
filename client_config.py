c3c39b50802250a857639ade7337a5659e607107
richmondu <richmond.umagat@yahoo.com>
2019-07-01 23:30:35 +0800

    Update client_config.py

--- client_config.py
+++ client_config.py
@@ -1,7 +1,9 @@
 class config:
 
+	CONFIG_USE_TLS  = True
 	CONFIG_HOST     = "localhost"
-	CONFIG_PORT     = 443
+	CONFIG_PORT     = 8000
+	CONFIG_TLS_PORT = 443
 	CONFIG_TLS_CA   = "cert/rootca.pem"
 	CONFIG_TLS_CERT = "cert/app_cert.pem"
 	CONFIG_TLS_PKEY = "cert/app_pkey.pem"
