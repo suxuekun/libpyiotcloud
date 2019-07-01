7dc2f7c3bdfee8b7af0f0f71ff4aac0ee3e2bf25
richmondu <richmond.umagat@yahoo.com>
2019-07-01 11:18:58 +0800

    Update batch file

--- device_simulator.js_mqtt_ft900device2.bat
+++ device_simulator.js_mqtt_ft900device2.bat
@@ -3,5 +3,9 @@ set DEVICE_CA="cert/rootca.pem"
 set DEVICE_CERT="cert/ft900device2_cert.pem"
 set DEVICE_PKEY="cert/ft900device2_pkey.pem"
 
-node device_simulator.js --USE_DEVICE_ID %DEVICE_ID% --USE_DEVICE_CA %DEVICE_CA% --USE_DEVICE_CERT %DEVICE_CERT% --USE_DEVICE_PKEY %DEVICE_PKEY%
+set HOST="localhost"
+set USER="guest"
+set PASS="guest"
+
+node device_simulator.js --USE_DEVICE_ID %DEVICE_ID% --USE_DEVICE_CA %DEVICE_CA% --USE_DEVICE_CERT %DEVICE_CERT% --USE_DEVICE_PKEY %DEVICE_PKEY% --USE_HOST %HOST% --USE_USERNAME %USER% --USE_PASSWORD %PASS%
 pause
