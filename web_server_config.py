d09a96bf53f861139c76148b88b6ef66437130b1
richmondu <richmond.umagat@yahoo.com>
2019-07-01 23:28:15 +0800

    Cleanup

--- web_server_config.py
+++ web_server_config.py
@@ -1,27 +1,34 @@
+import os
+
+
+
 class config:
 
-	# Web server settings
-	CONFIG_HTTP_USE_TLS         = True
-	CONFIG_HTTP_HOST            = "localhost"
-	CONFIG_HTTP_PORT            = 80
-	CONFIG_HTTP_TLS_PORT        = 443
-	CONFIG_HTTP_TLS_CA          = "cert/rootca.pem"
-	CONFIG_HTTP_TLS_CERT        = "cert/server_cert.pem"
-	CONFIG_HTTP_TLS_PKEY        = "cert/server_pkey.pem"
+    # Web server settings
+    if os.name == 'nt':
+        CONFIG_HTTP_USE_TLS     = True
+    else:
+        CONFIG_HTTP_USE_TLS     = False # Set to False when using GUnicorn (only in Linux)
+    CONFIG_HTTP_HOST            = "localhost"
+    CONFIG_HTTP_PORT            = 8000
+    CONFIG_HTTP_TLS_PORT        = 443
+    CONFIG_HTTP_TLS_CA          = "cert/rootca.pem"
+    CONFIG_HTTP_TLS_CERT        = "cert/server_cert.pem"
+    CONFIG_HTTP_TLS_PKEY        = "cert/server_pkey.pem"
 
-	# Message broker credentials
-	CONFIG_USERNAME             = "guest"
-	CONFIG_PASSWORD             = "guest"
-	CONFIG_TLS_CA               = "cert/rootca.pem"
-	CONFIG_TLS_CERT             = "cert/server_cert.pem"
-	CONFIG_TLS_PKEY             = "cert/server_pkey.pem"
+    # Message broker credentials
+    CONFIG_USERNAME             = "guest"
+    CONFIG_PASSWORD             = "guest"
+    CONFIG_TLS_CA               = "cert/rootca.pem"
+    CONFIG_TLS_CERT             = "cert/server_cert.pem"
+    CONFIG_TLS_PKEY             = "cert/server_pkey.pem"
 
-	# Message broker settings
-	CONFIG_USE_AMQP             = True
-	CONFIG_HOST                 = "localhost"
-	CONFIG_MQTT_TLS_PORT        = 8883
-	CONFIG_AMQP_TLS_PORT        = 5671
+    # Message broker settings
+    CONFIG_USE_AMQP             = True
+    CONFIG_HOST                 = "localhost"
+    CONFIG_MQTT_TLS_PORT        = 8883
+    CONFIG_AMQP_TLS_PORT        = 5671
 
-	# Database settings
-	CONFIG_DB_HOST              = "localhost"
-	CONFIG_DB_PORT              = 27017
+    # Database settings
+    CONFIG_DB_HOST              = "localhost"
+    CONFIG_DB_PORT              = 27017
