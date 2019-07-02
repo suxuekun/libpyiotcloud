5722dd777334cf21f8fc57d92e5b9b6156da4a84
richmondu <richmond.umagat@yahoo.com>
2019-07-02 19:00:52 +0800

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,5 @@
 from web_server import app
+from web_server_config import config
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host=config.CONFIG_HTTP_HOST, port=config.CONFIG_HTTP_PORT)
