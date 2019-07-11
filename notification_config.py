6d9a5e26863625f88610b48d80db502dd06af2a5
richmondu <richmond.umagat@yahoo.com>
2019-07-11 22:22:49 +0800

    Update notification_config.py

--- notification_config.py
+++ notification_config.py
@@ -1,8 +1,8 @@
 class config:
 
-    ACCESS_KEY      = ''
-    SECRET_KEY      = ''
-    PINPOINT_ID     = ''
-    PINPOINT_REGION = "us-east-1"
-    PINPOINT_EMAIL  = "richmond.umagat@gmail.com"
-
+    ACCESS_KEY             = ''
+    SECRET_KEY             = ''
+    PINPOINT_ID            = ''
+    PINPOINT_REGION        = "us-east-1"
+    PINPOINT_EMAIL         = "richmond.umagat@gmail.com"
+    PINPOINT_EMAIL_SUBJECT = "FT900 IoT Cloud Platform Notifications"
