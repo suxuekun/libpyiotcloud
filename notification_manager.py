806ebedc0a65976c30491e3b4e4874570479ed7d
richmondu <richmond.umagat@yahoo.com>
2019-07-11 19:27:15 +0800

    Add notification manager module

---
+++ notification_manager.py
@@ -0,0 +1,212 @@
+import json
+import time
+import netifaces
+import argparse
+import sys
+import boto3
+from messaging_client import messaging_client
+from notification_manager_config import notification_config
+
+
+
+###################################################################################
+# Enable to use AMQP for webserver-to-messagebroker communication
+# Disable to use MQTT for webserver-to-messagebroker communication
+CONFIG_USE_AMQP = False
+###################################################################################
+
+
+
+###################################################################################
+# global variables
+###################################################################################
+
+g_messaging_client = None
+g_notification_client = None
+g_gpio_values = {}
+
+
+###################################################################################
+# MQTT and AMQP default configurations
+###################################################################################
+
+CONFIG_DEVICE_ID            = ""
+
+CONFIG_USERNAME             = "guest"
+CONFIG_PASSWORD             = "guest"
+CONFIG_TLS_CA               = "cert/rootca.pem"
+CONFIG_TLS_CERT             = "cert/notification_manager_cert.pem"
+CONFIG_TLS_PKEY             = "cert/notification_manager_pkey.pem"
+
+CONFIG_HOST                 = "localhost"
+CONFIG_MQTT_TLS_PORT        = 8883
+CONFIG_AMQP_TLS_PORT        = 5671
+
+CONFIG_PREPEND_REPLY_TOPIC  = "server"
+CONFIG_SEPARATOR            = '/'
+
+
+###################################################################################
+# Notification configuration
+###################################################################################
+
+g_aws_access_key_id     = notification_config.ACCESS_KEY
+g_aws_secret_access_key = notification_config.SECRET_KEY
+g_pinpoint_project_id   = notification_config.PINPOINT_ID
+g_region_name           = notification_config.PINPOINT_REGION
+g_email_from            = notification_config.PINPOINT_EMAIL
+
+
+
+###################################################################################
+# Notification functions
+###################################################################################
+
+def send_email(pinpoint, email_recipient, email_message, email_subject):
+    response = pinpoint.send_messages(
+        ApplicationId=g_pinpoint_project_id,
+        MessageRequest={
+            'Addresses': {email_recipient: { 'ChannelType': 'EMAIL'}},
+            'MessageConfiguration': {
+                'EmailMessage': {
+                    'FromAddress': g_email_from,
+                    'SimpleEmail':  {
+                        'Subject':  {'Charset': 'UTF-8', 'Data': email_subject},
+                        'TextPart': {'Charset': 'UTF-8', 'Data': email_message}
+                    }
+                }
+            }
+        }
+    )
+    return response
+
+def send_sms(pinpoint, sms_recipient, sms_message):
+    response = pinpoint.send_messages(
+        ApplicationId=g_pinpoint_project_id,
+        MessageRequest={
+            'Addresses': {sms_recipient: {'ChannelType': 'SMS'}},
+            'MessageConfiguration': {
+                'SMSMessage': {
+                    'Body': sms_message
+                }
+            }
+        }
+    )
+    return response
+
+def send_message(pinpoint, recipient, message, subject=None):
+    if subject is None:
+        response = send_sms(pinpoint, recipient, message)
+    else:
+        response = send_email(pinpoint, recipient, message, subject)
+    return response
+
+
+###################################################################################
+# MQTT/AMQP callback functions
+###################################################################################
+
+def on_message(subtopic, subpayload):
+
+    payload = json.loads(subpayload)
+    recipient = payload["recipient"]
+    message = payload["message"]
+
+    is_email = True if recipient.find("@")!=-1 else False
+    subject = "FT900 IoT Cloud Platform Notifications" if is_email else None
+
+    response = send_message(g_notification_client, recipient, message, subject=subject)
+    print("\r\nSending message='{}' to recipient='{}' done. {} {}\r\n\r\n".format(
+        message, recipient,
+        response["ResponseMetadata"]["HTTPStatusCode"]==200, 
+        response["MessageResponse"]["Result"][recipient]["StatusCode"]==200))
+
+
+def on_mqtt_message(client, userdata, msg):
+
+    print("RCV: MQTT {} {}".format(msg.topic, msg.payload))
+    on_message(msg.topic, msg.payload)
+
+
+def on_amqp_message(ch, method, properties, body):
+
+    print("RCV: AMQP {} {}".format(method.routing_key, body))
+    on_message(method.routing_key, body)
+
+
+
+###################################################################################
+# Main entry point
+###################################################################################
+
+def parse_arguments(argv):
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--USE_AMQP', required=False, default=1 if CONFIG_USE_AMQP else 0, help='Use AMQP instead of MQTT')
+    parser.add_argument('--USE_DEVICE_ID', required=False, default=CONFIG_DEVICE_ID,    help='Device ID to use')
+    parser.add_argument('--USE_DEVICE_CA',   required=False, default=CONFIG_TLS_CA,   help='Device CA certificate to use')
+    parser.add_argument('--USE_DEVICE_CERT', required=False, default=CONFIG_TLS_CERT, help='Device certificate to use')
+    parser.add_argument('--USE_DEVICE_PKEY', required=False, default=CONFIG_TLS_PKEY, help='Device private key to use')
+    parser.add_argument('--USE_HOST',        required=False, default=CONFIG_HOST,     help='Host server to connect to')
+    parser.add_argument('--USE_USERNAME',    required=False, default=CONFIG_USERNAME, help='Username to use in connection')
+    parser.add_argument('--USE_PASSWORD',    required=False, default=CONFIG_PASSWORD, help='Password to use in connection')
+    return parser.parse_args(argv)
+
+
+if __name__ == '__main__':
+
+    args = parse_arguments(sys.argv[1:])
+    CONFIG_USE_AMQP    = True if int((args.USE_AMQP))==1 else False
+    CONFIG_SEPARATOR   = "." if int((args.USE_AMQP))==1 else "/"
+    CONFIG_DEVICE_ID   = args.USE_DEVICE_ID
+    CONFIG_TLS_CA      = args.USE_DEVICE_CA
+    CONFIG_TLS_CERT    = args.USE_DEVICE_CERT
+    CONFIG_TLS_PKEY    = args.USE_DEVICE_PKEY
+    CONFIG_HOST        = args.USE_HOST
+    CONFIG_USERNAME    = args.USE_USERNAME
+    CONFIG_PASSWORD    = args.USE_PASSWORD
+    print("")
+    print("USE_AMQP={}".format(args.USE_AMQP))
+    print("USE_DEVICE_ID={}".format(args.USE_DEVICE_ID))
+    print("USE_DEVICE_CA={}".format(args.USE_DEVICE_CA))
+    print("USE_DEVICE_CERT={}".format(args.USE_DEVICE_CERT))
+    print("USE_DEVICE_PKEY={}".format(args.USE_DEVICE_PKEY))
+    print("USE_HOST={}".format(args.USE_HOST))
+    print("USE_USERNAME={}".format(args.USE_USERNAME))
+    print("USE_PASSWORD={}".format(args.USE_PASSWORD))
+    print("")
+
+
+    # Initialize Notification (Pinpoint or SNS)
+    g_notification_client = boto3.Session(
+        aws_access_key_id=g_aws_access_key_id,
+        aws_secret_access_key=g_aws_secret_access_key,
+        region_name=g_region_name).client('pinpoint')
+
+
+    # Initialize MQTT/AMQP client
+    print("Using {} for device-messagebroker communication!".format("AMQP" if CONFIG_USE_AMQP else "MQTT"))
+    if CONFIG_USE_AMQP:
+        g_messaging_client = messaging_client(CONFIG_USE_AMQP, on_amqp_message, device_id=CONFIG_DEVICE_ID)
+        g_messaging_client.set_server(CONFIG_HOST, CONFIG_AMQP_TLS_PORT)
+    else:
+        g_messaging_client = messaging_client(CONFIG_USE_AMQP, on_mqtt_message, device_id=CONFIG_DEVICE_ID)
+        g_messaging_client.set_server(CONFIG_HOST, CONFIG_MQTT_TLS_PORT)
+    g_messaging_client.set_user_pass(CONFIG_USERNAME, CONFIG_PASSWORD)
+    g_messaging_client.set_tls(CONFIG_TLS_CA, CONFIG_TLS_CERT, CONFIG_TLS_PKEY)
+    try:
+        g_messaging_client.initialize()
+    except:
+        print("Could not connect to message broker")
+
+
+    # Subscribe to messages sent for this device
+    time.sleep(1)
+    subtopic = "{}{}+{}trigger_notification".format(CONFIG_PREPEND_REPLY_TOPIC, CONFIG_SEPARATOR, CONFIG_SEPARATOR)
+    g_messaging_client.subscribe(subtopic, subscribe=True, declare=True, consume_continuously=True)
+
+
+    while g_messaging_client.is_connected():
+        pass
+
+    print("application exits!")
