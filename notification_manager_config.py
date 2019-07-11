fcd07a0a2fe3c1a8966d3a61220b583142119964
richmondu <richmond.umagat@yahoo.com>
2019-07-11 19:27:43 +0800

    Create notification_manager_config.py

---
+++ notification_manager_config.py
@@ -0,0 +1,10 @@
+# This library should contain the access key and secret key
+
+class notification_config:
+
+    ACCESS_KEY      = ''
+    SECRET_KEY      = ''
+    PINPOINT_ID     = ''
+    PINPOINT_REGION = "us-east-1"
+    PINPOINT_EMAIL  = "richmond.umagat@gmail.com"
+
