46364134a91d84a4b4956f815f322f3d599b3a5c
richmondu <richmond.umagat@yahoo.com>
2019-07-15 23:33:44 +0800

    Remove username and password

--- device_simulator.py_mqtt_ft900device1.bat
+++ device_simulator.py_mqtt_ft900device1.bat
@@ -4,9 +4,7 @@ set DEVICE_CA="cert/rootca.pem"
 set DEVICE_CERT="cert/ft900device1_cert.pem"
 set DEVICE_PKEY="cert/ft900device1_pkey.pem"
 
-set HOST="localhost"
-set USER="guest"
-set PASS="guest"
+set HOST="192.168.100.11"
 
-python.exe device_simulator.py --USE_AMQP 0 --USE_DEVICE_ID %DEVICE_ID% --USE_DEVICE_CA %DEVICE_CA% --USE_DEVICE_CERT %DEVICE_CERT% --USE_DEVICE_PKEY %DEVICE_PKEY% --USE_HOST %HOST% --USE_USERNAME %USER% --USE_PASSWORD %PASS%
+python.exe device_simulator.py --USE_AMQP 0 --USE_DEVICE_ID %DEVICE_ID% --USE_DEVICE_CA %DEVICE_CA% --USE_DEVICE_CERT %DEVICE_CERT% --USE_DEVICE_PKEY %DEVICE_PKEY% --USE_HOST %HOST%
 pause
