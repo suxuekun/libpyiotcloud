d9020ee0e156a043475204504d875250e278f407
richmondu <richmond.umagat@yahoo.com>
2019-07-17 12:00:49 +0800

    Create notification_config.py

---
+++ notification_manager/notification_config.py
@@ -0,0 +1,12 @@
+class config:
+
+    ACCESS_KEY             = ""
+    SECRET_KEY             = ""
+
+    PINPOINT_ID            = ""
+    PINPOINT_REGION        = "us-east-1"
+    PINPOINT_EMAIL         = "richmond.umagat@gmail.com"
+    PINPOINT_EMAIL_SUBJECT = "FT900 IoT Cloud Platform Notifications"
+
+    SNS_TOPIC_ARN          = ""
+    SNS_REGION             = "ap-southeast-1"
