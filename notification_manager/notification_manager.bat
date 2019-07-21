211866720f517c0a24954856916eb96099dbb78d
richmondu <richmond.umagat@yahoo.com>
2019-07-21 23:50:41 +0800

    Update notification_manager.bat

--- notification_manager/notification_manager.bat
+++ notification_manager/notification_manager.bat
@@ -3,7 +3,7 @@ set DEVICE_CA="../cert/rootca.pem"
 set DEVICE_CERT="../cert/notification_manager_cert.pem"
 set DEVICE_PKEY="../cert/notification_manager_pkey.pem"
 
-set HOST="localhost"
+set HOST="192.168.99.100"
 
 python.exe notification_manager.py --USE_AMQP 0 --USE_DEVICE_ID %DEVICE_ID% --USE_DEVICE_CA %DEVICE_CA% --USE_DEVICE_CERT %DEVICE_CERT% --USE_DEVICE_PKEY %DEVICE_PKEY% --USE_HOST %HOST%
 pause
