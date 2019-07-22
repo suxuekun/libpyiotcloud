adef93ed5ad6d5dcdd1a22096b1fd843aba1eec6
richmondu <richmond.umagat@yahoo.com>
2019-07-22 18:16:03 +0800

    Clean up batch files

--- device_simulator/device_simulator.js_mqtt_ft900device1.bat
+++ device_simulator/device_simulator.js_mqtt_ft900device1.bat
@@ -1,13 +1,9 @@
-::set DEVICE_ID="9dfa09de9c360f6f7b61da26efd75cbb6e77526a"
-::set DEVICE_ID="37601281d3fcba385c22d1758cb5f96116db62d3"
-set DEVICE_ID="548e01758ec6292729592bb500ed37ea09eafee1"
+set DEVICE_ID="e2da8fffa35f8a470af3675e11a48966f2d6ddf6"
 set DEVICE_CA="../cert/rootca.pem"
 set DEVICE_CERT="../cert/ft900device1_cert.pem"
 set DEVICE_PKEY="../cert/ft900device1_pkey.pem"
 
-::set HOST="localhost"
-::set HOST="192.168.100.11"
-set HOST="ec2-13-229-115-250.ap-southeast-1.compute.amazonaws.com"
+set HOST="192.168.99.100"
 
 node device_simulator.js --USE_DEVICE_ID %DEVICE_ID% --USE_DEVICE_CA %DEVICE_CA% --USE_DEVICE_CERT %DEVICE_CERT% --USE_DEVICE_PKEY %DEVICE_PKEY% --USE_HOST %HOST%
 pause
--- device_simulator/device_simulator.py_mqtt_ft900device2.bat
+++ device_simulator/device_simulator.py_mqtt_ft900device2.bat
@@ -1,11 +1,9 @@
-::set DEVICE_ID="b92ac493b92eb996e53fcd534ab261bec7518e60"
-::set DEVICE_ID="bf9bd22361e66e2fef8e93de01652e22c400e6a7"
-set DEVICE_ID="b24b38d744b429e65be8f5ce76b75c60f6a61ac9"
+set DEVICE_ID="2ad771c84a1793351b31f4ffc69477949e337416"
 set DEVICE_CA="../cert/rootca.pem"
 set DEVICE_CERT="../cert/ft900device2_cert.pem"
 set DEVICE_PKEY="../cert/ft900device2_pkey.pem"
 
-set HOST="localhost"
+set HOST="192.168.99.100"
 
 python.exe device_simulator.py --USE_AMQP 0 --USE_DEVICE_ID %DEVICE_ID% --USE_DEVICE_CA %DEVICE_CA% --USE_DEVICE_CERT %DEVICE_CERT% --USE_DEVICE_PKEY %DEVICE_PKEY% --USE_HOST %HOST%
 pause
