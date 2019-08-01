67788a74a7153d99d015977a9675e0ce34a69a55
richmondu <richmond.umagat@yahoo.com>
2019-08-01 13:41:51 +0800

    Updated for quick testing

--- _device_simulator/device_simulator.js_mqtt_ft900device2.bat
+++ _device_simulator/device_simulator.js_mqtt_ft900device2.bat
@@ -1,9 +1,9 @@
-set DEVICE_ID="2ad771c84a1793351b31f4ffc69477949e337416"
+set DEVICE_ID="2a8d913bf29e8158c099e039973c2bba41178e86"
 set DEVICE_CA="cert/rootca.pem"
 set DEVICE_CERT="cert/ft900device2_cert.pem"
 set DEVICE_PKEY="cert/ft900device2_pkey.pem"
 
-set HOST="192.168.99.100"
+set HOST="ec2-54-85-32-68.compute-1.amazonaws.com"
 
 node device_simulator.js --USE_DEVICE_ID %DEVICE_ID% --USE_DEVICE_CA %DEVICE_CA% --USE_DEVICE_CERT %DEVICE_CERT% --USE_DEVICE_PKEY %DEVICE_PKEY% --USE_HOST %HOST%
 pause
--- _device_simulator/device_simulator.py_amqp_ft900device1.bat
+++ _device_simulator/device_simulator.py_amqp_ft900device1.bat
@@ -1,9 +1,9 @@
-set DEVICE_ID="e2da8fffa35f8a470af3675e11a48966f2d6ddf6"
+set DEVICE_ID="31a71e7c8877173123219dcc87c37db813954c1e"
 set DEVICE_CA="cert/rootca.pem"
 set DEVICE_CERT="cert/ft900device1_cert.pem"
 set DEVICE_PKEY="cert/ft900device1_pkey.pem"
 
-set HOST="192.168.99.100"
+set HOST="ec2-54-85-32-68.compute-1.amazonaws.com"
 
 python.exe device_simulator.py --USE_AMQP 1 --USE_DEVICE_ID %DEVICE_ID% --USE_DEVICE_CA %DEVICE_CA% --USE_DEVICE_CERT %DEVICE_CERT% --USE_DEVICE_PKEY %DEVICE_PKEY% --USE_HOST %HOST%
 pause
