ea6bead9c7c565001aa2e3d827e46863be1b8998
richmondu <richmond.umagat@yahoo.com>
2019-08-14 00:27:23 +0800

    Update server

--- _device_simulator/device_simulator.js_mqtt_ft900device2.bat
+++ _device_simulator/device_simulator.js_mqtt_ft900device2.bat
@@ -3,7 +3,8 @@ set DEVICE_CA="cert/rootca.pem"
 set DEVICE_CERT="cert/ft900device2_cert.pem"
 set DEVICE_PKEY="cert/ft900device2_pkey.pem"
 
-set HOST="ec2-54-85-32-68.compute-1.amazonaws.com"
+set HOST="richmondu.com"
+::set HOST="192.168.99.100"
 
 node device_simulator.js --USE_DEVICE_ID %DEVICE_ID% --USE_DEVICE_CA %DEVICE_CA% --USE_DEVICE_CERT %DEVICE_CERT% --USE_DEVICE_PKEY %DEVICE_PKEY% --USE_HOST %HOST%
 pause
