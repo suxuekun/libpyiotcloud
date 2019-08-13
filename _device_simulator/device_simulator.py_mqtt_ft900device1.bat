0fd249fab8eef42adc615970f4a72d3bf1c7cd2e
richmondu <richmond.umagat@yahoo.com>
2019-08-14 00:26:06 +0800

    Update device_simulator.py_mqtt_ft900device1.bat

--- _device_simulator/device_simulator.py_mqtt_ft900device1.bat
+++ _device_simulator/device_simulator.py_mqtt_ft900device1.bat
@@ -1,9 +1,10 @@
-set DEVICE_ID="31a71e7c8877173123219dcc87c37db813954c1e"
+set DEVICE_ID="83f58e3f7e284892f6da7b020e5c982a6ea16dee"
 set DEVICE_CA="cert/rootca.pem"
 set DEVICE_CERT="cert/ft900device1_cert.pem"
 set DEVICE_PKEY="cert/ft900device1_pkey.pem"
 
-set HOST="ec2-54-85-32-68.compute-1.amazonaws.com"
+set HOST="richmondu.com"
+::set HOST="192.168.99.100"
 
 python.exe device_simulator.py --USE_AMQP 0 --USE_DEVICE_ID %DEVICE_ID% --USE_DEVICE_CA %DEVICE_CA% --USE_DEVICE_CERT %DEVICE_CERT% --USE_DEVICE_PKEY %DEVICE_PKEY% --USE_HOST %HOST%
 pause
