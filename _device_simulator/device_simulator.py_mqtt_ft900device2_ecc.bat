2123554905f8e5479fa8d42f06f8fd1ae0272c92
richmondu <richmond.umagat@yahoo.com>
2019-09-25 20:33:23 +0800

    Revert "richmondu.com now points to ECC server"
    This reverts commit 947e4c9b4a0afce67acc0379d905b91f1e49781d.

--- _device_simulator/device_simulator.py_mqtt_ft900device2_ecc.bat
+++ _device_simulator/device_simulator.py_mqtt_ft900device2_ecc.bat
@@ -3,8 +3,8 @@ set DEVICE_CA="cert_ecc/rootca.pem"
 set DEVICE_CERT="cert_ecc/ft900device1_cert.pem"
 set DEVICE_PKEY="cert_ecc/ft900device1_pkey.pem"
 
-set HOST="richmondu.com"
-::set HOST="3.89.74.84"
+::set HOST="richmondu.com"
+set HOST="3.89.74.84"
 ::set HOST="ec2-3-89-74-84.compute-1.amazonaws.com"
 
 python.exe device_simulator.py --USE_AMQP 0 --USE_DEVICE_ID %DEVICE_ID% --USE_DEVICE_CA %DEVICE_CA% --USE_DEVICE_CERT %DEVICE_CERT% --USE_DEVICE_PKEY %DEVICE_PKEY% --USE_HOST %HOST%
