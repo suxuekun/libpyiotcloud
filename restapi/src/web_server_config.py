ab0980651156a04ae0f65dace400ee761fde10b0
richmondu <richmond.umagat@yahoo.com>
2019-09-27 11:26:16 +0800

    Run on Windows by default

--- restapi/src/web_server_config.py
+++ restapi/src/web_server_config.py
@@ -13,34 +13,40 @@ class config:
     CONFIG_HTTP_PORT            = 8000
     CONFIG_HTTP_TLS_PORT        = 443
     if int(os.environ["CONFIG_USE_ECC"])==1:
-        CONFIG_HTTP_TLS_CA          = "cert_ecc/rootca.pem"
-        CONFIG_HTTP_TLS_CERT        = "cert_ecc/server_cert.pem"
-        CONFIG_HTTP_TLS_PKEY        = "cert_ecc/server_pkey.pem"
+        CONFIG_HTTP_TLS_CA      = "cert_ecc/rootca.pem"
+        CONFIG_HTTP_TLS_CERT    = "cert_ecc/server_cert.pem"
+        CONFIG_HTTP_TLS_PKEY    = "cert_ecc/server_pkey.pem"
     else:
-        CONFIG_HTTP_TLS_CA          = "cert/rootca.pem"
-        CONFIG_HTTP_TLS_CERT        = "cert/server_cert.pem"
-        CONFIG_HTTP_TLS_PKEY        = "cert/server_pkey.pem"
+        CONFIG_HTTP_TLS_CA      = "cert/rootca.pem"
+        CONFIG_HTTP_TLS_CERT    = "cert/server_cert.pem"
+        CONFIG_HTTP_TLS_PKEY    = "cert/server_pkey.pem"
 
     # Message broker credentials
     CONFIG_USERNAME             = None
     CONFIG_PASSWORD             = None
     if int(os.environ["CONFIG_USE_ECC"])==1:
-        CONFIG_TLS_CA               = "cert_ecc/rootca.pem"
-        CONFIG_TLS_CERT             = "cert_ecc/server_cert.pem"
-        CONFIG_TLS_PKEY             = "cert_ecc/server_pkey.pem"
+        CONFIG_TLS_CA           = "cert_ecc/rootca.pem"
+        CONFIG_TLS_CERT         = "cert_ecc/server_cert.pem"
+        CONFIG_TLS_PKEY         = "cert_ecc/server_pkey.pem"
     else:
-        CONFIG_TLS_CA               = "cert/rootca.pem"
-        CONFIG_TLS_CERT             = "cert/server_cert.pem"
-        CONFIG_TLS_PKEY             = "cert/server_pkey.pem"
+        CONFIG_TLS_CA           = "cert/rootca.pem"
+        CONFIG_TLS_CERT         = "cert/server_cert.pem"
+        CONFIG_TLS_PKEY         = "cert/server_pkey.pem"
 
     # Message broker settings
     CONFIG_USE_AMQP             = False
-    CONFIG_HOST                 = "172.18.0.2"
+    if os.name == 'nt':
+        CONFIG_HOST             = "127.0.0.1"
+    else:
+        CONFIG_HOST             = "172.18.0.2"
     CONFIG_MQTT_TLS_PORT        = 8883
     CONFIG_AMQP_TLS_PORT        = 5671
 
     # Database settings
-    CONFIG_MONGODB_HOST         = "172.18.0.3"
+    if os.name == 'nt':
+        CONFIG_MONGODB_HOST     = "127.0.0.1"
+    else:
+        CONFIG_MONGODB_HOST     = "172.18.0.3"
     CONFIG_MONGODB_PORT         = 27017
     CONFIG_MONGODB_DB           = "iotcloud-database"
     CONFIG_MONGODB_TB_PROFILES  = "profiles"
--- webapp/src/ionicapp/www/js/server.js
+++ webapp/src/ionicapp/www/js/server.js
@@ -5,13 +5,12 @@ angular.module('server', [])
 .service('Server', [function(){
 
     //////////////////////////////////////////////////////////////////////////////////////////////
-    // TODO: UPDATE ME!!!
-    // This should point to the MACHINE ADDRESS, not the NGINX DOCKER ADDRESS (172.18.0.6)
-    // When using locally, https://192.168.99.100:443 will work!
-    // When using EC2, should contain the public IP address!
-    // When not using docker, this should point to the Flask REST API address
+    // TODO: UPDATE ME!!! This should point to the Flask REST API service
+    // When using on AWS EC2 docker, use https://richmondu.com or the EC2 public IP address
+    // When using locally on docker, use https://192.168.99.100:443
+    // When using locally on Windows, use https://localhost:443
     //////////////////////////////////////////////////////////////////////////////////////////////
-    var rest_api = 'https://192.168.99.100';
+    var rest_api = 'https://localhost';
 
     console.log(rest_api);
     var result = { 'rest_api': rest_api }
