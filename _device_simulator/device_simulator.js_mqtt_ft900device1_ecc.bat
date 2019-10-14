25d0d73b50d60a1db7f9034f59a0a51e39a5729c
richmondu <richmond.umagat@yahoo.com>
2019-10-14 14:41:39 +0800

    Add ECC scripts for Javascript

---
+++ _device_simulator/device_simulator.js_mqtt_ft900device1_ecc.bat
@@ -0,0 +1,10 @@
+set DEVICE_ID="1a077604c16008084c94f2d52e77f2be71bf8db2"
+set DEVICE_CA="cert_ecc/rootca.pem"
+set DEVICE_CERT="cert_ecc/ft900device1_cert.pem"
+set DEVICE_PKEY="cert_ecc/ft900device1_pkey.pem"
+
+set HOST="richmondu.com"
+::set HOST="192.168.99.100"
+
+node device_simulator.js --USE_DEVICE_ID %DEVICE_ID% --USE_DEVICE_CA %DEVICE_CA% --USE_DEVICE_CERT %DEVICE_CERT% --USE_DEVICE_PKEY %DEVICE_PKEY% --USE_HOST %HOST%
+pause
