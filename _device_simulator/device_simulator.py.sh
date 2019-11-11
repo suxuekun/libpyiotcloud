91629cc56a36cec7acb1eddecbda598004bdd403
Richmond Umagat <richmond.umagat@yahoo.com>
2019-11-12 00:22:15 +0800

    Added bash script for MacOS and Linux

---
+++ _device_simulator/device_simulator.py.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DEVICE_ID="2edbe5ed81fe2ac0f2f103db9751885264c98f97"
+DEVICE_CA="cert_ecc/rootca.pem"
+DEVICE_CERT="cert_ecc/ft900device1_cert.pem"
+DEVICE_PKEY="cert_ecc/ft900device1_pkey.pem"
+
+# HOST="richmondu.com"
+PORT="8883"
+# HOST="localhost"
+HOST="192.168.99.102"
+# PORT="30883"
+
+python.exe device_simulator.py --USE_ECC 1 --USE_AMQP 0 --USE_DEVICE_ID $DEVICE_ID --USE_DEVICE_CA $DEVICE_CA --USE_DEVICE_CERT $DEVICE_CERT --USE_DEVICE_PKEY $DEVICE_PKEY --USE_HOST $HOST --USE_PORT $PORT
+
