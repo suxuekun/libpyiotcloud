83a482f62a05134d2f214c941a9606179098289f
richmondu <richmond.umagat@yahoo.com>
2019-11-15 15:05:05 +0800

    Cleanup

--- notification/src/notification_manager/notification_manager_ecc.bat
+++ notification/src/notification_manager/notification_manager_ecc.bat
@@ -3,7 +3,7 @@ set DEVICE_CA="../cert_ecc/rootca.pem"
 set DEVICE_CERT="../cert_ecc/notification_manager_cert.pem"
 set DEVICE_PKEY="../cert_ecc/notification_manager_pkey.pem"
 
-set HOST="192.168.100.5"
+set HOST="localhost"
 
 python.exe notification_manager.py --USE_AMQP 0 --USE_DEVICE_ID %DEVICE_ID% --USE_DEVICE_CA %DEVICE_CA% --USE_DEVICE_CERT %DEVICE_CERT% --USE_DEVICE_PKEY %DEVICE_PKEY% --USE_HOST %HOST%
 pause
