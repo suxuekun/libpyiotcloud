4d66086cc4e21ade4a8f670c42fe5287fc94d479
richmondu <richmond.umagat@yahoo.com>
2019-11-15 15:30:21 +0800

    Enable setting username and password in scripts

--- _device_simulator/device_simulator.py.sh
+++ _device_simulator/device_simulator.py.sh
@@ -5,11 +5,17 @@ DEVICE_CA="cert_ecc/rootca.pem"
 DEVICE_CERT="cert_ecc/ft900device1_cert.pem"
 DEVICE_PKEY="cert_ecc/ft900device1_pkey.pem"
 
-# HOST="richmondu.com"
-PORT="8883"
-# HOST="localhost"
-HOST="192.168.99.102"
-# PORT="30883"
 
-python.exe device_simulator.py --USE_ECC 1 --USE_AMQP 0 --USE_DEVICE_ID $DEVICE_ID --USE_DEVICE_CA $DEVICE_CA --USE_DEVICE_CERT $DEVICE_CERT --USE_DEVICE_PKEY $DEVICE_PKEY --USE_HOST $HOST --USE_PORT $PORT
+HOST="richmondu.com"     #For production testing
+#HOST="localhost"        #For development testing with local non-Docker setup
+#HOST="192.168.99.100"   #For development testing with local Docker setup 
+
+PORT="8883"
+#PORT="30883"            #For development testing with Minikube Kubernetes
+
+USER=""
+PASS=""
+
+
+python.exe device_simulator.py --USE_ECC 1 --USE_AMQP 0 --USE_DEVICE_ID $DEVICE_ID --USE_DEVICE_CA $DEVICE_CA --USE_DEVICE_CERT $DEVICE_CERT --USE_DEVICE_PKEY $DEVICE_PKEY --USE_HOST $HOST --USE_PORT $PORT --USE_USERNAME $USER --USE_PASSWORD $PASS
 
