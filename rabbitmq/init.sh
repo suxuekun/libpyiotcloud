2a448c2ddedf635c50e2f9793bdaa4c186f1a3e3
richmondu <richmond.umagat@yahoo.com>
2019-11-25 22:00:50 +0800

    Update init.sh

--- rabbitmq/init.sh
+++ rabbitmq/init.sh
@@ -4,4 +4,5 @@
 rabbitmqctl add_user admin admin && \
 rabbitmqctl set_user_tags admin administrator && \
 rabbitmqctl set_permissions -p / admin ".*" ".*" ".*" && \
-rabbitmqctl set_topic_permissions -p / admin amq.topic "^$" "^$" )
+rabbitmqctl set_topic_permissions -p / admin amq.topic "^$" "^$" ) & \
+rabbitmq-server
