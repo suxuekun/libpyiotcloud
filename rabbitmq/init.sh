d995f765606ac38b7b3158069f6df07abaf84c40
richmondu <richmond.umagat@yahoo.com>
2019-11-25 22:08:24 +0800

    Revert security enforcement support

--- rabbitmq/init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-( sleep 10 && \
-rabbitmqctl add_user admin admin && \
-rabbitmqctl set_user_tags admin administrator && \
-rabbitmqctl set_permissions -p / admin ".*" ".*" ".*" && \
-rabbitmqctl set_topic_permissions -p / admin amq.topic "^$" "^$" ) & \
-rabbitmq-server
