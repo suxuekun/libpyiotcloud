c754a980464275e34dd364210edccee5fbdd970a
richmondu <richmond.umagat@yahoo.com>
2019-11-25 22:39:31 +0800

    Revert RabbitMQ set configuration

--- rabbitmq/init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-( sleep 5 && \
-rabbitmq-server start && \
-rabbitmqctl add_user admin admin && \
-rabbitmqctl set_user_tags admin administrator  && \
-rabbitmqctl set_permissions -p / admin ".*" ".*" ".*"  && \
-rabbitmqctl set_topic_permissions -p / admin amq.topic "^$" "^$" )
-
