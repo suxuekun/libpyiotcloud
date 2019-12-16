5ba65e2b4939ce95eb1280b94e6a02bc0c0dfc4d
richmondu <richmond.umagat@yahoo.com>
2019-12-16 17:32:07 +0800

    Updated notification manager to read from database
    1. Updated notification manager to read from database when device does not provide the recipient and/or message.
    -  uses provided recipient and/or message (if provided)
    -  uses notification information saved from database when recipient and/or message is NOT provided by using DEVICEID

--- notification/src/notification_manager/notification_manager_ecc.bat
+++ notification/src/notification_manager/notification_manager_ecc.bat
@@ -4,6 +4,7 @@ set DEVICE_CERT="../cert_ecc/notification_manager_cert.pem"
 set DEVICE_PKEY="../cert_ecc/notification_manager_pkey.pem"
 
 set HOST="localhost"
+set DBHOST="localhost"
 
-python.exe notification_manager.py --USE_AMQP 0 --USE_DEVICE_ID %DEVICE_ID% --USE_DEVICE_CA %DEVICE_CA% --USE_DEVICE_CERT %DEVICE_CERT% --USE_DEVICE_PKEY %DEVICE_PKEY% --USE_HOST %HOST%
+python.exe notification_manager.py --USE_AMQP 0 --USE_DEVICE_ID %DEVICE_ID% --USE_DEVICE_CA %DEVICE_CA% --USE_DEVICE_CERT %DEVICE_CERT% --USE_DEVICE_PKEY %DEVICE_PKEY% --USE_HOST %HOST% --USE_DBHOST %DBHOST%
 pause
