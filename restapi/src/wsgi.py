f131adfbeddd5f1ca89894a03ccf6439c2226331
richmondu <richmond.umagat@yahoo.com>
2020-02-27 01:42:57 +0800

    Update wsgi.py

--- restapi/src/wsgi.py
+++ restapi/src/wsgi.py
@@ -2,4 +2,4 @@ from rest_api import app
 from rest_api_config import config
 
 if __name__ == "__main__":
-    app.run(host=config.CONFIG_HTTP_HOST, port=config.CONFIG_HTTP_PORT, threaded=True, debug=False)
+    app.run(host=config.CONFIG_HTTP_HOST, port=config.CONFIG_HTTP_PORT, threaded=True, debug=True)
