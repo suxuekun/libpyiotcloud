3129a8bf32bc179b00f3738f00ae27fd647938b8
richmondu <richmond.umagat@yahoo.com>
2020-02-29 00:59:28 +0800

    Support login via google

--- webapp/src/ionicapp/www/js/env.js
+++ webapp/src/ionicapp/www/js/env.js
@@ -6,6 +6,6 @@
     window.__env.apiUrl = "localhost";
     window.__env.jwtKey = "iotmodembrtchip0iotmodembrtchip0";
     window.__env.googleApiKey = "googleapikey";
-    window.__env.oauthDomain = "awsoauthdomain";
-    window.__env.clientId = "awsclientid";
+    window.__env.oauthDomain = "ft900iotportal.auth.ap-southeast-1.amazoncognito.com";
+    window.__env.clientId = "2023qso66in7l7ipglti99f0a8";
 }(this));
