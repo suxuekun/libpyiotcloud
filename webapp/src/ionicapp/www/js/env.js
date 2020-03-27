709aad2fecdbe3650d8ad3df732003a677aceaa2
richmondu <richmond.umagat@yahoo.com>
2020-03-27 17:36:22 +0800

    Update env.js

--- webapp/src/ionicapp/www/js/env.js
+++ webapp/src/ionicapp/www/js/env.js
@@ -5,7 +5,7 @@
     window.__env = window.__env || {};
     window.__env.apiUrl = "localhost";
     window.__env.jwtKey = "iotmodembrtchip0iotmodembrtchip0";
-    window.__env.googleApiKey = "AIzaSyDv-LTkTOY1DEztInPX_I0XgbBzQ1gGozg";
-    window.__env.oauthDomain = "ft900iotportal.auth.ap-southeast-1.amazoncognito.com";
-    window.__env.clientId = "2023qso66in7l7ipglti99f0a8";
+    window.__env.googleApiKey = "googleapikey";
+    window.__env.oauthDomain = "awsoauthdomain";
+    window.__env.clientId = "awsclientid";
 }(this));
