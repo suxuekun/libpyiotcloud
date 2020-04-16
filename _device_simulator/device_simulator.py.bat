d17bf20a5f5c972b4e6e0df41d5d88273d87a44a
richmondu <richmond.umagat@yahoo.com>
2020-04-16 17:09:28 +0800

    Supported enhanced device authentication using JWT-encoded password

--- _device_simulator/device_simulator.py.bat
+++ _device_simulator/device_simulator.py.bat
@@ -1,19 +1,33 @@
-set DEVICE_ID="PH80XXRR112219EF"
+:::::::::::::::::::::::::::
+:: Device credentials
+:::::::::::::::::::::::::::
+set DEVICE_ID=""
+set DEVICE_SERIAL=""
+set DEVICE_MACADD=""
+set DEVICE_SECRETKEY=""
+::NO NEED TO SPECIFY USERNAME AND PASSWORD NO. Application will generate based on DEVICE_ID, DEVICE_SERIAL, DEVICE_MACADD
+::set USER=""
+::set PASS=""
+
+
+:::::::::::::::::::::::::::
+:: Device certificates
+:::::::::::::::::::::::::::
 set DEVICE_CA="cert_ecc/rootca.pem"
 set DEVICE_CERT="cert_ecc/ft900device1_cert.pem"
 set DEVICE_PKEY="cert_ecc/ft900device1_pkey.pem"
 
 
-set HOST="richmondu.com"
-::set HOST="localhost"
-::set HOST="192.168.99.100"
-
+:::::::::::::::::::::::::::
+:: Backend message broker
+:::::::::::::::::::::::::::
 set PORT="8883"
+set HOST="ec2-54-88-41-145.compute-1.amazonaws.com"
+::set HOST="localhost"
+::set HOST="richmondu.com"
+::set HOST="192.168.99.100"
 ::set PORT="30883"
 
-set USER=""
-set PASS=""
 
-
-python.exe device_simulator.py --USE_ECC 1 --USE_AMQP 0 --USE_DEVICE_ID %DEVICE_ID% --USE_DEVICE_CA %DEVICE_CA% --USE_DEVICE_CERT %DEVICE_CERT% --USE_DEVICE_PKEY %DEVICE_PKEY% --USE_HOST %HOST% --USE_PORT %PORT% --USE_USERNAME %USER% --USE_PASSWORD %PASS%
+python.exe device_simulator.py --USE_ECC 1 --USE_AMQP 0 --USE_DEVICE_ID %DEVICE_ID% --USE_DEVICE_CA %DEVICE_CA% --USE_DEVICE_CERT %DEVICE_CERT% --USE_DEVICE_PKEY %DEVICE_PKEY% --USE_HOST %HOST% --USE_PORT %PORT% --USE_DEVICE_SERIAL %DEVICE_SERIAL% --USE_DEVICE_MACADD %DEVICE_MACADD% --USE_DEVICE_SECRETKEY %DEVICE_SECRETKEY% --USE_USERNAME %USER% --USE_PASSWORD %PASS%
 pause
