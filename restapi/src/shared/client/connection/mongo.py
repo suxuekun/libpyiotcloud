5973f180e88b4abd74de632109755547f54286d3
xuekun su <suxuekun@gmail.com>
2020-06-05 10:28:39 +0800

    1 change defaultmongodb using defaultmongoconnection ,not extend mongoconnection

--- restapi/src/shared/client/connection/mongo.py
+++ restapi/src/shared/client/connection/mongo.py
@@ -2,13 +2,7 @@ from pymongo import MongoClient
 
 from rest_api_config import config
 from shared.client.connection.base import IotDBConnection
-from shared.utils.singleton_util import Singleton
 
-
-class DefaultMongoConnection(IotDBConnection,metaclass=Singleton):
+class DefaultMongoConnection(IotDBConnection):
     def __init__(self):
         self._conn = MongoClient(config.CONFIG_MONGODB_HOST, config.CONFIG_MONGODB_PORT)
-
-
-
-
--- restapi/src/shared/client/db/mongo/default.py
+++ restapi/src/shared/client/db/mongo/default.py
@@ -1,10 +1,12 @@
 from rest_api_config import config
+from shared.client.connection.base import IotDBConnection
 from shared.client.connection.mongo import DefaultMongoConnection
 from shared.client.db.mongo.base import IotMongoDB
 
 
-class DefaultMongoDB(DefaultMongoConnection,IotMongoDB):
+class DefaultMongoDB(IotDBConnection,IotMongoDB):
     def __init__(self):
         super(DefaultMongoDB,self).__init__()
+        self._conn = DefaultMongoConnection().conn
         self._db = self.conn[config.CONFIG_MONGODB_DB]
 
