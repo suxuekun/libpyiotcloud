77f183a880db135bf578719471cee541fcef1978
xuekun su <suxuekun@gmail.com>
2020-06-10 09:35:36 +0800

    fix example module import payment bugs

--- restapi/src/example_module/__init__.py
+++
@@ -1,6 +0,0 @@
-from payment.routes import payment_blueprint
-class PaymentApp:
-    PREFIX="/example"
-    def __init__(self, app):
-        app.register_blueprint(payment_blueprint, url_prefix=self.PREFIX)
-
