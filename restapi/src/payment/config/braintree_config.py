687eff42a02db060af4fedd4b3c7dc04baf0c4d6
xuekun su <suxuekun@gmail.com>
2020-06-17 03:17:51 +0800

    remove string , tomorrow will try remove history

--- restapi/src/payment/config/braintree_config.py
+++ restapi/src/payment/config/braintree_config.py
@@ -1,5 +1,5 @@
 CONFIG_MODE='sandbox'
-CONFIG_MERCHANT_ID='hwrcsdskt8rdt9tq'
-CONFIG_PUBLIC_KEY='5jvxdtrrkrjtmssd'
-CONFIG_PRIVATE_KEY='044dcebb3f347bcd757d04821247c339'
+CONFIG_MERCHANT_ID=''
+CONFIG_PUBLIC_KEY=''
+CONFIG_PRIVATE_KEY=''
 # APP_SECRET_KEY='your unique secret key; a long, random set of characters'
