2b09d65d762b1fa0b53db4d0a05ffb63d05a1e24
xuekun su <suxuekun@gmail.com>
2020-06-29 18:24:19 +0800

    test db transaction

--- restapi/src/shared/client/db/mongo/test.py
+++ restapi/src/shared/client/db/mongo/test.py
@@ -9,3 +9,23 @@ class TestMongoDB(IotDBConnection,IotMongoDB):
         self._conn = TestMongoConnection().conn
         self._db = self.conn['iotcloud-database-test']
 
+
+if __name__ == '__main__':
+    db = TestMongoDB()
+    client = db.conn
+    orders = client.db.orders
+    inventory = client.db.inventory
+    with client.start_session() as session:
+        with session.start_transaction():
+            orders.insert_one({"sku": "abc123", "qty": 100}, session=session)
+            inventory.update_one({"sku": "abc123", "qty": {"$gte": 100}},
+                                 {"$inc": {"qty": -100}}, session=session)
+
+            item = orders.find_one({"sku": "abc123"})
+            print(item)
+            raise Exception('wtf')
+
+    item = orders.find_one({"sku": "abc123"})
+    print(item)
+
+
