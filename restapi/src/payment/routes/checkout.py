c704153fc4d9176db243c9e189b21c36c22a1c5d
xuekun su <suxuekun@gmail.com>
2020-06-30 22:19:31 +0800

    fix checkout api route naming of request_params

--- restapi/src/payment/routes/checkout.py
+++ restapi/src/payment/routes/checkout.py
@@ -15,25 +15,26 @@ class CheckoutResource(Resource,BaseResource):
     service = payment_service
     wrapper_class = IotHttpResponseWrapper
     POSTDTO = CheckoutDTO
-    # @throw_custome_error_request()
+    @throw_custome_error_request()
     def post(self):
+        data = None
         try:
             request = getRequest()
-            data = request.get_json()
+            req_data = request.get_json()
             username = informations.get_entityname(request)
             # query = informations.get_entityname_query(request)
-            dto =self.to_valid_request_data(data)
+            dto =self.to_valid_request_data(req_data)
             dto.validate()
         except Exception as e:
             print(e)
-            make_error_response(http4xx.BAD_REQUEST)
+            return make_error_response(http4xx.BAD_REQUEST)
 
-        print(dto.to_primitive())
-        # try:
-        data = self.service.checkout(username,dto.nonce,dto.items);
-        # except Exception as e:
-        #     print(e)
-        #     return make_custom_error_response({'status': 'NG', 'message': 'check out fail with braintree'}, 503)
+        # print(dto.to_primitive())
+        try:
+            data = self.service.checkout(username,dto.nonce,dto.items);
+        except Exception as e:
+            print(e)
+            return make_custom_error_response({'status': 'NG', 'message': 'check out fail with braintree'}, 503)
         if data:
             res = self.to_api_data(data)
             return self.to_result(res)
@@ -46,8 +47,8 @@ class CancelSubscriptionResource(Resource,BaseResource):
     wrapper_class = IotHttpResponseWrapper
     def post(self):
         request = getRequest()
-        data = request.get_json()
-        subscription_id = data.get('subscription_id')
+        req_data = request.get_json()
+        subscription_id = req_data.get('subscription_id')
         data = self.service.cancel_subscription(subscription_id)
         if data:
             res = self.to_api_data(data)
