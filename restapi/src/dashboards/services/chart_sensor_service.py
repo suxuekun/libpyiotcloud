bc72ffa217d59d6eb8efe80de3f3afeef47c867a
richmondu <richmond.umagat@yahoo.com>
2020-07-21 14:45:33 +0800

    Add dashboard update from Thang - gatewayName and enabled parameters

--- restapi/src/dashboards/services/chart_sensor_service.py
+++ restapi/src/dashboards/services/chart_sensor_service.py
@@ -264,6 +264,8 @@ class ChartSensorService:
 
             chartEntity = self.chartRepository.getById(chartId)
             sensor = self.sensorRepository.getById(chartEntity["deviceId"])
+            gateway = self.deviceRepository.get_by_uuid(sensor["deviceid"]);
+            sensor["gateway"] = gateway
 
             calculateMultiTimeRange = 1
             if query.isRealtime == False:
