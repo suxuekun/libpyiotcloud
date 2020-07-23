5544b7a42471715ea1cfea2161319c0d613c9e52
richmondu <richmond.umagat@yahoo.com>
2020-07-23 15:38:22 +0800

    Cleanup

--- _device_simulator/device_simulator_fleet_automation.py
+++ _device_simulator/device_simulator_fleet_automation.py
@@ -68,6 +68,113 @@ def printf_json(json_object, label=None):
         printf(json_formatted_str)
 
 ###################################################################################
+# File Operations
+###################################################################################
+
+def read_file(filename):
+    try:
+        f = open(filename, "r")
+        contents = f.read()
+        f.close()
+    except Exception as e:
+        return None
+
+    return contents
+
+def write_file(filename, contents):
+    try:
+        f = open(filename, "w")
+        f.write(contents)
+        f.close()
+    except Exception as e:
+        return None
+
+    return contents
+
+###################################################################################
+# Device simulator scripts
+###################################################################################
+
+def read_script_templates():
+    bat = read_file("templates/device_simulator.py.bat")
+    sh = read_file("templates/device_simulator.py.sh")
+    return bat, sh
+
+def generate_script_file(host, template, ext, devicename, deviceid, serialnumber, poemacaddress):
+    filename = "device_simulator.py.{}.{}".format(devicename, ext)
+
+    contents = ""
+    lines = template.split("\n")
+    for line in lines:
+        if "DEVICE_ID=\"\"" in line:
+            contents += line.replace("DEVICE_ID=\"\"", "DEVICE_ID=\"" + deviceid + "\"")
+        elif "DEVICE_SERIAL=\"\"" in line:
+            contents += line.replace("DEVICE_SERIAL=\"\"", "DEVICE_SERIAL=\"" + serialnumber + "\"")
+        elif "DEVICE_MACADD=\"\"" in line:
+            contents += line.replace("DEVICE_MACADD=\"\"", "DEVICE_MACADD=\"" + poemacaddress + "\"")
+        elif "HOST=\"\"" in line:
+            contents += line.replace("HOST=\"\"", "HOST=\"" + host + "\"")
+        else:
+            contents += line
+        contents += "\n"
+    write_file(filename, contents)
+    return filename
+
+def generate_script_files(host, bat_template, sh_template, devicename, deviceid, serialnumber, poemacaddress):
+    if os.name == "nt":
+        filename = generate_script_file(host, bat_template, "bat", devicename, deviceid, serialnumber, poemacaddress)
+    else:
+        filename = generate_script_file(host, sh_template, "sh", devicename, deviceid, serialnumber, poemacaddress)
+    return filename
+
+def generate_script_master(host, devicename_prefix, numdevices):
+    if os.name == "nt":
+        ext = "bat"
+        filename = "device_simulator.py.{}_ALL.{}".format(devicename_prefix, ext)
+        contents = ""
+        for index in range(numdevices):
+            contents += "START device_simulator.py.{}_{:03}.{}\n".format(devicename_prefix, index, ext)
+        write_file(filename, contents)
+    else:
+        # TODO
+        pass
+
+def run_script_file(filename):
+    if os.name == "nt":
+        os.system("START {}".format(filename))
+    else:
+        # TODO
+        os.system("START {}".format(filename))
+
+###################################################################################
+# Device generation
+###################################################################################
+
+def generate_deviceid(index, key=0):
+    year = datetime.now().year - 2000
+    month = datetime.now().month
+    day = datetime.now().day
+    deviceid = "PH80XX{:02X}{:02}{:02}{:02}{:02X}".format(key, month, day, year, index)
+    return deviceid
+
+def generate_serialnumber():
+    serialnumber = random.randint(10000, 99999)
+    return str(serialnumber)
+
+def generate_macaddress():
+    macaddress = ""
+    for x in range(6):
+        macaddress += "{:02X}:".format(random.randint(0, 255))
+    return macaddress[:-1]
+
+def generate_device(devicename_prefix, index, key=0):
+    devicename = "{}_{:03}".format(devicename_prefix, index)
+    deviceid = generate_deviceid(index, key)
+    serialnumber = generate_serialnumber()
+    macaddress = generate_macaddress()
+    return devicename, deviceid, serialnumber, macaddress
+
+###################################################################################
 # HTTP Utilities
 ###################################################################################
 
@@ -127,12 +234,9 @@ def http_recv_response(conn, debug=True):
         printf("\tRES: Could not communicate with DEVICE! {}".format(e))
     return 0, None
 
-
-
 ###################################################################################
 # HTTP APIs
 ###################################################################################
-
 
 def encode_jwt(username, password, secret_key):
     iat = int(time.time())
@@ -281,100 +385,9 @@ def get_device_status(host, port, tokens, devicename):
     result, value = http_send_receive(host, port, method, api, params, headers, "value")
     return result
 
-def generate_deviceid(index):
-    year = datetime.now().year - 2000
-    month = datetime.now().month
-    day = datetime.now().day
-    deviceid = "PH80XX00{:02}{:02}{:02}{:02X}".format(month, day, year, index)
-    return deviceid
-
-def generate_serialnumber():
-    serialnumber = random.randint(10000, 99999)
-    return str(serialnumber)
-
-def generate_macaddress():
-    macaddress = ""
-    for x in range(6):
-        macaddress += "{:02X}:".format(random.randint(0, 255))
-    return macaddress[:-1]
-
-def generate_device(devicename_prefix, index):
-    devicename = "{}_{:03}".format(devicename_prefix, index)
-    deviceid = generate_deviceid(index)
-    serialnumber = generate_serialnumber()
-    macaddress = generate_macaddress()
-    return devicename, deviceid, serialnumber, macaddress
-
-def read_file(filename):
-    try:
-        f = open(filename, "r")
-        contents = f.read()
-        f.close()
-    except Exception as e:
-        return None
-
-    return contents
-
-def write_file(filename, contents):
-    try:
-        f = open(filename, "w")
-        f.write(contents)
-        f.close()
-    except Exception as e:
-        return None
-
-    return contents
-
-def read_script_templates():
-    bat = read_file("templates/device_simulator.py.bat")
-    sh = read_file("templates/device_simulator.py.sh")
-    return bat, sh
-
-def generate_script_file(host, template, ext, devicename, deviceid, serialnumber, poemacaddress):
-    filename = "device_simulator.py.{}.{}".format(devicename, ext)
-
-    contents = ""
-    lines = template.split("\n")
-    for line in lines:
-        if "DEVICE_ID=\"\"" in line:
-            contents += line.replace("DEVICE_ID=\"\"", "DEVICE_ID=\"" + deviceid + "\"")
-        elif "DEVICE_SERIAL=\"\"" in line:
-            contents += line.replace("DEVICE_SERIAL=\"\"", "DEVICE_SERIAL=\"" + serialnumber + "\"")
-        elif "DEVICE_MACADD=\"\"" in line:
-            contents += line.replace("DEVICE_MACADD=\"\"", "DEVICE_MACADD=\"" + poemacaddress + "\"")
-        elif "HOST=\"\"" in line:
-            contents += line.replace("HOST=\"\"", "HOST=\"" + host + "\"")
-        else:
-            contents += line
-        contents += "\n"
-    write_file(filename, contents)
-    return filename
-
-def generate_script_files(host, bat_template, sh_template, devicename, deviceid, serialnumber, poemacaddress):
-    if os.name == "nt":
-        filename = generate_script_file(host, bat_template, "bat", devicename, deviceid, serialnumber, poemacaddress)
-    else:
-        filename = generate_script_file(host, sh_template, "sh", devicename, deviceid, serialnumber, poemacaddress)
-    return filename
-
-def generate_script_master(host, devicename_prefix, numdevices):
-    if os.name == "nt":
-        ext = "bat"
-        filename = "device_simulator.py.{}_ALL.{}".format(devicename_prefix, ext)
-        contents = ""
-        for index in range(numdevices):
-            contents += "START device_simulator.py.{}_{:03}.{}\n".format(devicename_prefix, index, ext)
-        write_file(filename, contents)
-    else:
-        # TODO
-        pass
-
-def run_script_file(filename):
-    if os.name == "nt":
-        os.system("START {}".format(filename));
-    else:
-        # TODO
-        os.system("START {}".format(filename));
+###################################################################################
+# Argument Parsing
+###################################################################################
 
 def parse_arguments(argv):
     parser = argparse.ArgumentParser()
@@ -385,17 +398,14 @@ def parse_arguments(argv):
     parser.add_argument('--USE_DEVICE_SECRETKEY', required=True, default='', help='HTTP secret key')
     parser.add_argument('--USE_DEVICE_COUNT',     required=True, default='', help='Number of devices to create run')
     parser.add_argument('--USE_DEVICE_NAMEPREFIX',required=True, default='', help='Name prefix to be used')
+    parser.add_argument('--USE_UID_KEY',          required=True, default='', help='UID key for deviceid')
     return parser.parse_args(argv)
 
-def main(args):
-    global printf
+###################################################################################
+# Application info display
+###################################################################################
 
-    printf = setup_logging("device_simulator_fleet_automation_logs.txt")
-    host = args.USE_HOST
-    port = int(args.USE_PORT)
-    numdevices = int(args.USE_DEVICE_COUNT)
-    devicename_prefix = args.USE_DEVICE_NAMEPREFIX
-
+def display_info():
     printf("-------------------------------------------------------")
     printf("Copyright (C) Bridgetek Pte Ltd")
     printf("-------------------------------------------------------")
@@ -414,7 +424,24 @@ def main(args):
     printf("")
     printf("-------------------------------------------------------")
 
+###################################################################################
+# Main application logic
+###################################################################################
 
+def main(args):
+
+    global printf
+    printf = setup_logging("device_simulator_fleet_automation_logs.txt")
+    display_info()
+
+    # get parameters
+    host = args.USE_HOST
+    port = int(args.USE_PORT)
+    numdevices = int(args.USE_DEVICE_COUNT)
+    devicename_prefix = args.USE_DEVICE_NAMEPREFIX
+    uid_key = int(args.USE_UID_KEY)
+
+    # login to retrieve authentication tokens
     tokens = login(host, port, args.USE_USERNAME, args.USE_PASSWORD, args.USE_DEVICE_SECRETKEY)
     if tokens is None:
         return
@@ -424,18 +451,21 @@ def main(args):
     printf("Strating registration and running of {} devices...".format(numdevices))
     printf("")
     for index in range(numdevices):
-        # generate device information
-        devicename, deviceid, serialnumber, poemacaddress = generate_device(devicename_prefix, index)
-        #devicename = "Dev1"
 
-        # add device
+        # generate device information
+        # then add device
+        devicename, deviceid, serialnumber, poemacaddress = generate_device(devicename_prefix, index)
         result = add_device(host, port, tokens, devicename, deviceid, serialnumber, poemacaddress)
         if not result:
             # if device already exists, get device information
             device = get_device(host, port, tokens, devicename)
             if device is None:
                 printf("Error: device does not exist!")
-                return
+                # try with a new key and repeat the index
+                uid_key += 1
+                index -= 1
+                continue
+            # save the device information
             deviceid = device["deviceid"]
             serialnumber = device["serialnumber"]
             poemacaddress = device["poemacaddress"]
@@ -445,11 +475,12 @@ def main(args):
             printf("{:03} {} {} {}".format(index, devicename, deviceid, serialnumber, poemacaddress))
             printf("\tregistered...")
 
-
-        # run device simulators
+        # create the script for the device simulator
+        # then execute the script to run the device simulators
         filename = generate_script_files(host, bat_template, sh_template, devicename, deviceid, serialnumber, poemacaddress)
         run_script_file(filename)
 
+        # get device status
         time.sleep(1)
         while True:
             time.sleep(1)
@@ -462,7 +493,6 @@ def main(args):
         # get sensors
         busport = "1"
         sensors = get_sensors_by_port(host, port, tokens, devicename, busport)
-
         for sensor in sensors:
             result = True
             # set configuration if not configured
@@ -471,7 +501,6 @@ def main(args):
                 result = set_sensor_configuration(host, port, tokens, devicename, sensor, config["notification"])
                 if not result:
                     printf("ERROR: Sensor configure failed!")
-
             # enable sensors if not enabled
             if result:
                 printf("\t{} {} sensor configured - {}".format(sensor["source"], sensor["number"], sensor["class"]))
@@ -482,6 +511,7 @@ def main(args):
                         continue
                 printf("\t{} {} sensor enabled - {}".format(sensor["source"], sensor["number"], sensor["class"]))
 
+    # generate the master script for all the device script
     generate_script_master(host, devicename_prefix, numdevices)
     printf("Completed registration and running of {} devices.".format(numdevices))
 
